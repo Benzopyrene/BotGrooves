0e26da499165effed9c01d8454292aa715645d94
Benzopyrene <102141782+Benzopyrene@users.noreply.github.com>
2023-04-02 22:23:47 +0100

    New member messages are done

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -2,9 +2,13 @@ module.exports = {
 	name: 'guildMemberAdd',
 	execute: async(member, client) => {
 
-		if (member.guild.id == '1007959559400325210') { // Dev server
-			console.log('A new member has joined the server!');
+		if (member.guild.id == '1090678203334529075') { // Dev server
 
+			const welcomeChannel = client.channels.cache.get('1092194769079386272');
+			console.log(member);
+			welcomeChannel.send({
+				content: `${member.user.username} has joined the vibe`
+			}); // member.user.id   member.user._roles   member.user.bot
 		}
 	},
 };
