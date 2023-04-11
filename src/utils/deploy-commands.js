c32434e83a69d5608f32af5264722fee667d4604
Benzopyrene <102141782+Benzopyrene@users.noreply.github.com>
2023-04-11 20:45:11 +0100

    tidying this up

--- src/utils/deploy-commands.js
+++ src/utils/deploy-commands.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 const commands = [];
 
-let commandFiles
-let commandsFolders
+let commandFiles;
+let commandsFolders;
 
-const guildCommand = true; // Toggle to prevent dev commands going global
+const guildCommand = true; // TOGGLE HERE to prevent dev commands going global
 
 if (guildId == '1090678203334529075' && guildCommand) { // Dev server (deliberately hardcoded)
 	commandsFolders = fs.readdirSync(`./src/commands`); // All command folders & files
