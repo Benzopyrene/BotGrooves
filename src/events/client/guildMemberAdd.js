ee303d87ec67522b8883a81d21a05cd8a9de71a6
Benzopyrene <102141782+Benzopyrene@users.noreply.github.com>
2023-05-21 21:36:32 +0100

    activating the guildMamberAdd file

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -4,17 +4,17 @@ module.exports = {
 
 		if (member.guild.id == '838606384895492096') { // Main server
 
-			// const welcomeChannel = client.channels.cache.get('838631101698015252'); // Currently #welcome
+			const welcomeChannel = client.channels.cache.get('838631101698015252'); // Currently #welcome
 
-			// // Welcome message
-			// await welcomeChannel.send({
-			// 	content: `${member.user.username} has joined the vibe`
-			// }); 
+			// Welcome message
+			await welcomeChannel.send({
+				content: `${member.user.username} has joined the vibe`
+			}); 
 
-			// // Welcome DM
-			// member.user.send({
-			// 	content: 'Welcome to the vibe my homie, enjoy your stay, and keep those booties movin\''
-			// });
+			// Welcome DM
+			member.user.send({
+				content: 'Welcome to the vibe my homie, enjoy your stay, and keep those booties movin\''
+			});
 		}
 	},
 };
